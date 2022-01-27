abe0ecae6a067f0d199f7b0960127303e2d64245
James Jameson <james.jmson@gmail.com>
2022-01-27 05:30:15 +0100

    Expand comments

--- ch-1/016.c
+++ ch-1/016.c
@@ -12,8 +12,21 @@ int main(){
     }
     //The program awaits for input (text stream) at c=getchar().
     //Let 12\n be the stream after program execution, \n being Enter/new line.
-    //\n "triggers the execution of the first getchar()".
-    //c=getchar()='c'=99, see $ man ascii.
+    //After having pressed Enter (\n), the first getchar() reads the 1 from the text stream.
+    //c=getchar()='1'=49, see $ man ascii.
     //The program loops until it prints the whole stream 12\n and then it awaits for input.
-    //Ctrl d triggers getchar() again, whose value is EOF; this breaks the while loop.
+    //Ctrl d, if pressed, triggers getchar() again, whose value is EOF; this breaks the while loop.
+
+    // This way you can read the decimal ASCII value for the first character of the text stream.
+    // Try: 1\n
+
+    // int c=getchar();
+    // printf("~~~\n");
+    // printf("Decimal value: %d\n", c);
+    // putchar(c);
+    // printf("\n");
+    // return 0;
+
+    // printf("%d\n", EOF);
+    // -1
 }
