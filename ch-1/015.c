4bbba180afa10c02ab8e549f49f5c97ec8b73f79
James Jameson <james.jmson@gmail.com>
2022-01-27 04:19:20 +0100

    Remove comment, add printf alternative

--- ch-1/015.c
+++ ch-1/015.c
@@ -8,9 +8,10 @@
 
 int main(){
     for(int fahr=LOWER; fahr<=UPPER; fahr+=STEP){
-        // printf("%d\t%6.1f\n", fahr, 5*(fahr-32)/9.);
         printf("%d\t%6.1f\n", fahr, 5*(fahr-32)/9.);
         //Tabs have different width on terminal.
         //I suspect 8 spaces minus %d width
+
+        // printf("%6d%6.1f\n", fahr, 5*(fahr-32)/9.);
     }
 }
